e8b44362bd97fecd596125de09e1aa8cb73a1633
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-06-14 16:01:22 -0300

    PlaylistTest.java feito e testado

--- test/br/edu/insper/desagil/backend/model/PlaylistTest.java
+++ test/br/edu/insper/desagil/backend/model/PlaylistTest.java
@@ -5,30 +5,70 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PlaylistTest {
+class PlaylistTest 
+{
+	
 	private static double DELTA = 0.000001;
+	private Artist a;
+	private Track t;
+	private Playlist pl;
 
 	@BeforeEach
-	void setUp() {
+	void setUp() 
+	{
+		a = new Artist("Anitta");
+		t = new Track(a, "t", 300);
+		pl = new Playlist(1);
+		pl.addTrack(t);
 	}
 
 	@Test
-	void testRoundDownToZero() {
-		assertEquals(true, false);
+	void testRoundDownToZero() 
+	{
+		pl.putRating("user1", 1);
+		pl.putRating("user2", 2);
+		pl.putRating("user3", 3);
+		pl.putRating("user4", 3);
+		
+		double mean = pl.averageRatings();
+		double expected = 2.0;
+		assertEquals(expected, mean, DELTA);
 	}
 
 	@Test
-	void testRoundUpToHalf() {
-		assertEquals(true, false);
+	void testRoundUpToHalf() 
+	{
+		pl.putRating("user1", 1);
+		pl.putRating("user2", 2);
+		pl.putRating("user3", 1);
+		
+		double mean = pl.averageRatings();
+		double expected = 1.5;
+		assertEquals(expected, mean, DELTA);
 	}
 
 	@Test
-	void testRoundDownToHalf() {
-		assertEquals(true, false);
+	void testRoundDownToHalf() 
+	{
+		pl.putRating("user1", 1);
+		pl.putRating("user2", 2);
+		pl.putRating("user3", 2);
+		
+		double mean = pl.averageRatings();
+		double expected = 1.5;
+		assertEquals(expected, mean, DELTA);
 	}
 
 	@Test
-	void testRoundUpToOne() {
-		assertEquals(true, false);
+	void testRoundUpToOne() 
+	{
+		pl.putRating("user1", 1);
+		pl.putRating("user2", 1);
+		pl.putRating("user3", 2);
+		pl.putRating("user4", 3);
+		
+		double mean = pl.averageRatings();
+		double expected = 2.0;
+		assertEquals(expected, mean, DELTA);
 	}
 }
